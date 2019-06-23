25ed61e45e91fac111336c4ec989dfbb98b6225f
Shane <shanemcneil19@gmail.com>
2019-06-22 22:05:13 -0700

    complete with a little style, that's how I roll

--- index.js
+++ index.js
@@ -6,20 +6,35 @@ function getImages() {
     .then(response => response.json())
     .then(responseJson => displayResults(responseJson))
     .catch(error => console.log('Something went wrong.  Try again later.'));
+
+    
 }
 
 function displayResults(responseJson) {
     console.log(responseJson);
+    
+    let images = responseJson.message;
+    console.log(images);
+    let dogPic = ``;
+
+    for(let i=0; i<images.length; i++) {
+        dogPic = `<img src="${images[i]}" class="results-img" alt="dog picture">`;
+        console.log(dogPic);
+
+        $('.results').append(dogPic);
+    };
 }
 
 function watchForm() {
     $('form').submit(e => {
         e.preventDefault();
+        $('.results').html("");
         getImages();
     });
 }
 
 $(function() {
     console.log("Ok, let's look at some dogs!");
+    
     watchForm();
 });
